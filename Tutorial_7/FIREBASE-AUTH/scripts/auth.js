79e65586d8a911a44c0a44107de489b88b4b4bd8
KobengChantel <kobengchantel@gmail.com>
2024-05-31 07:07:53 +0200

    push

--- Tutorial_7/FIREBASE-AUTH/scripts/auth.js
+++ Tutorial_7/FIREBASE-AUTH/scripts/auth.js
@@ -1,47 +1,15 @@
-// add admin cloud function
-const adminForm = document.querySelector('.admin-actions');
-adminForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  const adminEmail = document.querySelector('#admin-email').value;
-  const addAdminRole = functions.httpsCallable('addAdminRole');
-  addAdminRole({ email: adminEmail }).then(result => {
-    console.log(result);
-  });
+db.collection('guides').get().then(snapshot => {
+  setupGuides(snapshot.docs);
 });
 
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    user.getIdTokenResult().then(idTokenResult => {
-      user.admin = idTokenResult.claims.admin;
-      setupUI(user);
-    });
-    db.collection('guides').onSnapshot(snapshot => {
-      setupGuides(snapshot.docs);
-    }, err => console.log(err.message));
+    console.log('user logged in: ', user);
   } else {
-    setupUI();
-    setupGuides([]);
+    console.log('user logged out');
   }
-});
-
-// create new guide
-const createForm = document.querySelector('#create-form');
-createForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  db.collection('guides').add({
-    title: createForm.title.value,
-    content: createForm.content.value
-  }).then(() => {
-    // close the create modal & reset form
-    const modal = document.querySelector('#modal-create');
-    M.Modal.getInstance(modal).close();
-    createForm.reset();
-  }).catch(err => {
-    console.log(err.message);
-  });
-});
+})
 
 // signup
 const signupForm = document.querySelector('#signup-form');
@@ -52,19 +20,12 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user & add firestore data
+  // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    return db.collection('users').doc(cred.user.uid).set({
-      bio: signupForm['signup-bio'].value
-    });
-  }).then(() => {
     // close the signup modal & reset form
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
-    signupForm.querySelector('.error').innerHTML = ''
-  }).catch(err => {
-    signupForm.querySelector('.error').innerHTML = err.message;
   });
 });
 
@@ -90,9 +51,6 @@ loginForm.addEventListener('submit', (e) => {
     const modal = document.querySelector('#modal-login');
     M.Modal.getInstance(modal).close();
     loginForm.reset();
-    loginForm.querySelector('.error').innerHTML = '';
-  }).catch(err => {
-    loginForm.querySelector('.error').innerHTML = err.message;
   });
 
 });
