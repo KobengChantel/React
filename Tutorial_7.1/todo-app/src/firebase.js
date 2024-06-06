ab58ec23576dc7188cbe9fa93ae89a552a0bb67b
KobengChantel <kobengchantel@gmail.com>
2024-06-06 11:19:12 +0200

    push

--- Tutorial_7.1/todo-app/src/firebase.js
+++ Tutorial_7.1/todo-app/src/firebase.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+// const firebaseConfig = {
 
-  apiKey: "AIzaSyCCiQN9jAn3cQSRb3LlHR6qISt7Ets7XuY",
-  authDomain: "todo-app-562ca.firebaseapp.com",
-  projectId: "todo-app-562ca",
-  storageBucket: "todo-app-562ca.appspot.com",
-  messagingSenderId: "757270408529",
-  appId: "1:757270408529:web:e656ef13eb01bbc111b770",
-  measurementId: "G-FQ9W61X8JX"
-};
+//   apiKey: "AIzaSyCCiQN9jAn3cQSRb3LlHR6qISt7Ets7XuY",
+//   authDomain: "todo-app-562ca.firebaseapp.com",
+//   projectId: "todo-app-562ca",
+//   storageBucket: "todo-app-562ca.appspot.com",
+//   messagingSenderId: "757270408529",
+//   appId: "1:757270408529:web:e656ef13eb01bbc111b770",
+//   measurementId: "G-FQ9W61X8JX"
+// };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
